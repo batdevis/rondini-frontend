e4dbfa75f84b791b7fd846d830e95b5705a2557d
Devis Battisti <devis.battisti@uqido.com>
2020-11-19 12:06:43 +0100

    multilingual

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/css');
 
+  eleventyConfig.addHandlebarsShortcode("localeSwitch", function(locale) {
+    return `/${locale}${this.page.url.substr(3)}`;
+  });
+
   return {
     dir: {
       input: "src",
