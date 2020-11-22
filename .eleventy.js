a68c41e3c395efae87556aa1fe6904e6783676ff
Devis Battisti <devis.battisti@uqido.com>
2020-11-22 02:08:45 +0100

    menu links

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,23 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/css');
 
-  eleventyConfig.addHandlebarsShortcode("localeSwitch", function(locale) {
-    return `/${locale}${this.page.url.substr(3)}`;
+  eleventyConfig.addHandlebarsShortcode("pgUrl", function(pg, locale, options) {
+    return `${locale}/${pg.slug[locale]}`;
+  });
+
+  eleventyConfig.addHandlebarsShortcode("pgLink", function(pg, locale, options) {
+    const href = `${locale}/${pg.slug[locale]}`;
+    const text = pg.title[locale];
+    const tag = `<a href=${href}>${text}</a>`;
+    return tag;
+  });
+
+  eleventyConfig.addHandlebarsShortcode("isActive", function(pg, currentPg, options) {
+    return (pg.name == currentPg.name);
+  });
+
+  eleventyConfig.addHandlebarsShortcode("logOptions", function(options) {
+    console.log(options.data.root);
   });
 
   return {
